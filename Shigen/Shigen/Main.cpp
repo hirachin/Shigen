b9d06eedc8d15c1805556c16ac42c56bdd93164e
kenazu <hirachin0113@gmail.com>
2019-03-04 17:14:53 +0900

    時間経過で資源が増加するようにした。

--- Shigen/Shigen/Main.cpp
+++ Shigen/Shigen/Main.cpp
@@ -6,11 +6,15 @@
 void Main()
 {
 	Resource resource(L"Data/Resource.ini");
-	resource.saveIni(L"Data/Resource.ini");
+	//resource.saveIni(L"Data/Resource.ini");
 
 	while (System::Update())
 	{
 		ClearPrint();
 		Println(resource);
+		Println(DateTime::Now().format(L"HH:mm:ss"));
+		Println(resource.getTimeStamp().format(L"HH:mm:ss"));
+
+		resource.update();
 	}
 }
--- Shigen/Shigen/Resource.h
+++ Shigen/Shigen/Resource.h
@@ -9,15 +9,50 @@ class Resource
 	int m_c;
 	int m_d;
 
+	DateTime m_timeStamp;
+
+	FilePath m_path;
+
+	DateTime StringToDateTime(const String& _dateStr)
+	{
+		const auto splitDateTime = _dateStr.split(L' ');
+
+		if (splitDateTime.size() != 2) { return DateTime(); }
+
+		const auto splitDate = splitDateTime[0].split(L'/');
+		const auto splitTime = splitDateTime[1].split(L':');
+
+		if (splitDate.size() != 3 || splitTime.size() != 3) { return DateTime(); }
+
+		const int32 year = Parse<int32>(splitDate[0]);
+		const int32 month = Parse<int32>(splitDate[1]);
+		const int32 date = Parse<int32>(splitDate[2]);
+
+		const int32 hour = Parse<int32>(splitTime[0]);
+		const int32 min = Parse<int32>(splitTime[1]);
+		const int32 sec = Parse<int32>(splitTime[2]);
+
+		const DateTime datetime(year, month, date, hour, min, sec);
+
+		return datetime;
+	}
+
+
 public:
 
-	Resource() :m_a(0), m_b(0), m_c(0), m_d(0) {}
+	Resource() :m_a(0), m_b(0), m_c(0), m_d(0),m_timeStamp(DateTime::Now()) {}
 
-	Resource(const FilePath& _path)
+	Resource(const FilePath& _path):m_path(_path)
 	{
 		loadIni(_path);
 	}
 
+	~Resource()
+	{
+		saveIni(m_path);
+	}
+
+	DateTime getTimeStamp()const { return m_timeStamp; }
 
 	bool loadIni(const FilePath& _path)
 	{
@@ -29,6 +64,7 @@ public:
 		m_b = ini.getOr<int>(L"Resource.b", 0);
 		m_c = ini.getOr<int>(L"Resource.c", 0);
 		m_d = ini.getOr<int>(L"Resource.d", 0);
+		m_timeStamp = StringToDateTime(ini.getOr<String>(L"Resource.timeStamp", DateTime::Now().format()));
 		ini.close();
 		return true;
 	}
@@ -41,6 +77,8 @@ public:
 		ini.write(L"Resource", L"b", m_b);
 		ini.write(L"Resource", L"c", m_c);
 		ini.write(L"Resource", L"d", m_d);
+		ini.write(L"Resource", L"timeStamp", m_timeStamp.format());
+
 		ini.close();
 		return true;
 	}
@@ -49,6 +87,31 @@ public:
 	int getB()const { return m_b; }
 	int getC()const { return m_c; }
 	int getD()const { return m_d; }
+
+
+	void update()
+	{
+		const auto duration_ms = DateTime::Now() - m_timeStamp;
+		const auto duration_min = DurationCast<Minutes, Microseconds>(duration_ms);
+		
+		const auto interval_min = Minutes(1);
+
+
+		if (duration_min >= interval_min)
+		{
+			for (auto d_min = duration_min; d_min >= interval_min; d_min -= interval_min)
+			{
+				m_a += 3;
+				m_b += 3;
+				m_c += 3;
+				m_d += 1;
+				m_timeStamp = DateTime::Now();
+			}
+		}
+
+		
+
+	}
 };
 
 template <class CharType>
